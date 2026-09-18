5ffaf137aa7031be9160c8322bccfb1cedc74cee
eragon-09 <rotmilan11@gmail.com>
2026-09-18 19:23:15 +0200

    ok

--- matrix/update_repo.py
+++ matrix/update_repo.py
@@ -1,97 +1,153 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# *
-# *  Copyright (C) 2012-2013 Garrett Brown
-# *  Copyright (C) 2010      j48antialias
-# *
-# *  This Program is free software; you can redistribute it and/or modify
-# *  it under the terms of the GNU General Public License as published by
-# *  the Free Software Foundation; either version 2, or (at your option)
-# *  any later version.
-# *
-# *  This Program is distributed in the hope that it will be useful,
-# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# *  GNU General Public License for more details.
-# *
-# *  You should have received a copy of the GNU General Public License
-# *  along with XBMC; see the file COPYING.  If not, write to
-# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-# *  http://www.gnu.org/copyleft/gpl.html
-# *
-# *  Based on code by j48antialias:
-# *  https://anarchintosh-projects.googlecode.com/files/addons_xml_generator.py
-
 """
-addons.xml generator – bereinigte Version
+Kodi addons.xml generator with support for repository folders whose name
+is different from the add-on id (e.g. binary add-ons such as
+inputstream.ffmpegdirect+windows-x86_64).
+
+For such folders Kodi needs <path> inside xbmc.addon.metadata / kodi.addon.metadata.
+The path is only injected into the generated addons.xml; source addon.xml files
+are not modified.
 """
 
 import os
-import sys
 import hashlib
+import xml.etree.ElementTree as ET
 
-# Pfade festlegen
-dir_path = os.path.dirname(os.path.realpath(__file__))
-addonsxml_file = os.path.join(dir_path, "addons.xml")
-addons_md5 = os.path.join(dir_path, "addons.xml.md5")
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+ADDONS_XML_FILE = os.path.join(DIR_PATH, "addons.xml")
+ADDONS_MD5_FILE = os.path.join(DIR_PATH, "addons.xml.md5")
 
-def u(x):
-    return x  # Für Python 3 keine Umwandlung nötig
+METADATA_POINTS = {"xbmc.addon.metadata", "kodi.addon.metadata"}
+
 
 class Generator:
-    """
-    Generiert eine neue addons.xml-Datei aus allen addon.xml-Dateien im Ordner
-    und erstellt eine passende MD5-Hashdatei dazu.
-    """
-
     def __init__(self):
         self._generate_addons_file()
         self._generate_md5_file()
         print("✔ Fertig: addons.xml und addons.xml.md5 wurden aktualisiert.")
 
+    @staticmethod
+    def _indent(elem, level=0):
+        """Simple pretty-printer compatible with older Python 3 versions."""
+        indent = "\n" + level * "  "
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = indent + "  "
+            for child in elem:
+                Generator._indent(child, level + 1)
+            if not elem[-1].tail or not elem[-1].tail.strip():
+                elem[-1].tail = indent
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = indent
+
+    @staticmethod
+    def _find_metadata_extension(root):
+        for ext in root.findall("extension"):
+            if ext.get("point") in METADATA_POINTS:
+                return ext
+        return None
+
+    @staticmethod
+    def _inject_repository_path(root, folder_name, addon_path):
+        """
+        Add repository-only <path> when physical folder != addon id.
+
+        Kodi interprets this path relative to <datadir>.  Example:
+          inputstream.ffmpegdirect+windows-x86_64/
+          inputstream.ffmpegdirect-21.3.8.zip
+        """
+        addon_id = root.get("id")
+        version = root.get("version")
+        if not addon_id or not version:
+            raise ValueError("addon.xml enthält keine gültige id/version")
+
+        if folder_name == addon_id:
+            return False
+
+        metadata = Generator._find_metadata_extension(root)
+        if metadata is None:
+            raise ValueError("keine xbmc.addon.metadata/kodi.addon.metadata Extension gefunden")
+
+        zip_name = f"{addon_id}-{version}.zip"
+        zip_path = os.path.join(addon_path, zip_name)
+        repo_path = f"{folder_name}/{zip_name}"
+
+        # Replace an existing repository-only path to avoid duplicates.
+        old_path = metadata.find("path")
+        if old_path is not None:
+            metadata.remove(old_path)
+
+        path_element = ET.Element("path")
+        path_element.text = repo_path
+        metadata.insert(0, path_element)
+
+        if os.path.isfile(zip_path):
+            print(f"  ↳ Pfad: {addon_id} -> {repo_path}")
+        else:
+            print(f"⚠ ZIP fehlt: {zip_path}")
+            print(f"  ↳ Pfad wird trotzdem eingetragen: {repo_path}")
+
+        return True
+
     def _generate_addons_file(self):
-        addons = os.listdir(dir_path)
-        addons_xml = u('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<addons>\n')
+        addons_root = ET.Element("addons")
+        count = 0
+        mapped = 0
 
-        for addon in addons:
-            addon_path = os.path.join(dir_path, addon)
+        for folder_name in sorted(os.listdir(DIR_PATH), key=str.lower):
+            addon_path = os.path.join(DIR_PATH, folder_name)
+
+            if not os.path.isdir(addon_path):
+                continue
+            if folder_name.endswith((".svn", ".git")):
+                continue
+
+            addon_xml_path = os.path.join(addon_path, "addon.xml")
+            if not os.path.isfile(addon_xml_path):
+                # Same behavior as the old script, but without noisy traceback.
+                continue
+
             try:
-                if (not os.path.isdir(addon_path) or addon.endswith(".svn") or addon.endswith(".git")):
-                    continue
+                tree = ET.parse(addon_xml_path)
+                root = tree.getroot()
 
-                addon_xml_path = os.path.join(addon_path, "addon.xml")
-                with open(addon_xml_path, "r", encoding="utf-8") as f:
-                    xml_lines = f.read().splitlines()
+                if root.tag != "addon":
+                    raise ValueError("Root-Element ist nicht <addon>")
 
-                addon_xml = ""
-                for line in xml_lines:
-                    if "<?xml" in line:
-                        continue
-                    addon_xml += line.rstrip() + "\n"
+                if self._inject_repository_path(root, folder_name, addon_path):
+                    mapped += 1
 
-                addons_xml += addon_xml.rstrip() + "\n\n"
+                addons_root.append(root)
+                count += 1
 
-            except Exception as e:
-                print(f"⚠ Ausschluss von {addon_path} wegen Fehler: {e}")
+            except Exception as exc:
+                print(f"⚠ Ausschluss von {addon_path} wegen Fehler: {exc}")
 
-        addons_xml = addons_xml.strip() + u("\n</addons>\n")
-        self._save_file(addons_xml.encode("utf-8"), file=addonsxml_file)
+        self._indent(addons_root)
+        xml_body = ET.tostring(addons_root, encoding="unicode", short_empty_elements=True)
+        content = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' + xml_body + "\n"
+        self._save_file(content.encode("utf-8"), ADDONS_XML_FILE)
+
+        print(f"✔ {count} Add-ons eingelesen, {mapped} abweichende Repository-Pfade eingetragen.")
 
     def _generate_md5_file(self):
         try:
-            with open(addonsxml_file, "r", encoding="utf-8") as f:
-                content = f.read().encode("utf-8")
-            m = hashlib.md5(content).hexdigest()
-            self._save_file(m.encode("utf-8"), file=addons_md5)
-        except Exception as e:
-            print(f"❌ Fehler beim Erstellen von addons.xml.md5: {e}")
+            with open(ADDONS_XML_FILE, "rb") as f:
+                digest = hashlib.md5(f.read()).hexdigest()
+            self._save_file(digest.encode("ascii"), ADDONS_MD5_FILE)
+        except Exception as exc:
+            print(f"❌ Fehler beim Erstellen von addons.xml.md5: {exc}")
 
-    def _save_file(self, data, file):
+    @staticmethod
+    def _save_file(data, filename):
         try:
-            with open(file, "wb") as f:
+            with open(filename, "wb") as f:
                 f.write(data)
-        except Exception as e:
-            print(f"❌ Fehler beim Speichern von {file}: {e}")
+        except Exception as exc:
+            print(f"❌ Fehler beim Speichern von {filename}: {exc}")
+            raise
+
 
 if __name__ == "__main__":
     Generator()
